b4fa11a00f0ba5ee0307d19ab6ff473e5f279fff
Himanshu Yadav (1000138607) <YADAH3@student.op.ac.nz>
2026-05-13 14:33:34 +1200

    Some Customization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to {name}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Hi There , Welcome to my website</p>
+
+<style>
+    p{
+        color: red;
+        font: bold;
+        text-align: center;
+
+    }
+    h1{
+        color: rgb(255, 145, 0);
+        text-align: center;
+    }
+    
+</style>
+
+<script>
+    name = "Web1-Class"
+</script>
